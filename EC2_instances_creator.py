ec093b0689c7110854779cfc9fe63e79a408c75e
Ismail <bakkouriismail@gmail.com>
2022-10-11 14:40:02 -0400

    Implemented class to create EC2 instances & clusters

---
+++ EC2_instances_creator.py
@@ -0,0 +1,83 @@
+import boto3
+import constant
+
+class EC2Creator:
+    def __init__(self):
+        self.client = boto3.client(
+            'ec2',
+            region_name="us-east-1",
+            aws_access_key_id="ASIAYZCTG2ALWHUZFK54",
+            aws_secret_access_key="C2RNAzD4FKUNNzYXYlCspmAbkTLZ1uiAychd5JcZ",
+            aws_session_token="FwoGZXIvYXdzEMP//////////wEaDP3leF14mDovpTXIxCLDAX52lezY4yeLYR/xp5mizNRQsw3jt1V0B/nLv73RgxFonmNYzPCGZQ5q2kbYztCiwabHJQY00GY9nZtqh3122HbodrE5gJJ/OHF6x2xboALFN5eNH1J0ZvmnaoIxkmNX9IeTRh7egr3E6OMcYod+Gs1l5+K+RWk511x/8X3zXivBGiFzZottb0cPf1eE/VSPNraf0t8NhBAfxYnYm1zfaIH/uAAWYOLUUzB9CDMwDe55K8Ic/L7F9hlKIuMYRYPVfiJQXii+15aaBjIt+EPWYeqsHbEBsImv+GgQObeq57U+LhCDqF7JM3ki3TWJmpzNDFRZO1MXpIiB"
+        )
+
+        self.cluster_t2_instances_ids = []
+        self.cluster_m4_instances_ids = []
+
+    def create_instance(self, availability_zone, instance_type):
+        response = self.client.run_instances(
+            BlockDeviceMappings=[
+                {
+                    'DeviceName': '/dev/sda1',
+                    'Ebs': {
+                        # deleting the storage on instance termination
+                        'DeleteOnTermination': True,
+                        # 8gb volume
+                        'VolumeSize': 8,
+                        'VolumeType': 'gp2',
+                    },
+                },
+            ],
+            # UBUNTU instance
+            ImageId=constant.UBUNTU_IMAGE,
+            # UBUNTU instance
+            InstanceType=instance_type,
+            # default key_pair
+            KeyName=constant.DEFAULT_KEY_PAIR,
+
+            # Availability zone
+            Placement={
+                'AvailabilityZone': availability_zone,
+            },
+
+            # Security groups
+            SecurityGroups=[
+                constant.DEFAULT_SECURITY_GROUP,
+            ],
+            DisableApiTermination=False,
+
+            # One instance
+            MaxCount=1,
+            MinCount=1
+        )
+        print(response["Instances"][0]["InstanceId"])
+        return response["Instances"][0]["InstanceId"]
+
+    def create_cluster_t2_large(self):
+        self.cluster_t2_instances_ids = [
+            self.create_instance(constant.US_EAST_1A, constant.T2_LARGE),
+            self.create_instance(constant.US_EAST_1B, constant.T2_LARGE),
+            self.create_instance(constant.US_EAST_1C, constant.T2_LARGE),
+            self.create_instance(constant.US_EAST_1D, constant.T2_LARGE),
+            self.create_instance(constant.US_EAST_1E, constant.T2_LARGE),
+        ]
+
+    def create_cluster_m4_large(self):
+        self.cluster_m4_instances_ids = [
+            self.create_instance(constant.US_EAST_1A, constant.M4_LARGE),
+            self.create_instance(constant.US_EAST_1B, constant.M4_LARGE),
+            self.create_instance(constant.US_EAST_1C, constant.M4_LARGE),
+            self.create_instance(constant.US_EAST_1D, constant.M4_LARGE),
+            self.create_instance(constant.US_EAST_1E, constant.M4_LARGE),
+        ]
+
+    def create_clusters(self):
+        self.create_cluster_t2_large()
+        self.create_cluster_m4_large()
+
+
+obj = EC2Creator()
+obj.create_clusters()
+
+print(obj.cluster_t2_instances_ids)
+print(obj.cluster_m4_instances_ids)
---
+++ constant.py
@@ -0,0 +1,14 @@
+# EC2
+UBUNTU_IMAGE = "ami-08c40ec9ead489470"
+US_EAST_1A = "us-east-1a"
+US_EAST_1B = "us-east-1b"
+US_EAST_1C = "us-east-1c"
+US_EAST_1D = "us-east-1d"
+US_EAST_1E = "us-east-1e"
+
+T2_LARGE = "t2.large"
+M4_LARGE = "m4.large"
+
+DEFAULT_KEY_PAIR = "key1"
+
+DEFAULT_SECURITY_GROUP = "launch-wizard-1"
