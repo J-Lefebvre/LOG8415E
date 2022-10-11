cd15cdcb76f72f2b6c6cf70484092c47c8083935
Ismail <bakkouriismail@gmail.com>
2022-10-11 14:51:57 -0400

    removed credentials

--- EC2_instances_creator.py
+++ EC2_instances_creator.py
@@ -6,9 +6,9 @@ class EC2Creator:
         self.client = boto3.client(
             'ec2',
             region_name="us-east-1",
-            aws_access_key_id="ASIAYZCTG2ALWHUZFK54",
-            aws_secret_access_key="C2RNAzD4FKUNNzYXYlCspmAbkTLZ1uiAychd5JcZ",
-            aws_session_token="FwoGZXIvYXdzEMP//////////wEaDP3leF14mDovpTXIxCLDAX52lezY4yeLYR/xp5mizNRQsw3jt1V0B/nLv73RgxFonmNYzPCGZQ5q2kbYztCiwabHJQY00GY9nZtqh3122HbodrE5gJJ/OHF6x2xboALFN5eNH1J0ZvmnaoIxkmNX9IeTRh7egr3E6OMcYod+Gs1l5+K+RWk511x/8X3zXivBGiFzZottb0cPf1eE/VSPNraf0t8NhBAfxYnYm1zfaIH/uAAWYOLUUzB9CDMwDe55K8Ic/L7F9hlKIuMYRYPVfiJQXii+15aaBjIt+EPWYeqsHbEBsImv+GgQObeq57U+LhCDqF7JM3ki3TWJmpzNDFRZO1MXpIiB"
+            aws_access_key_id="",
+            aws_secret_access_key="",
+            aws_session_token=""
         )
 
         self.cluster_t2_instances_ids = []
@@ -22,8 +22,11 @@ class EC2Creator:
                     'Ebs': {
                         # deleting the storage on instance termination
                         'DeleteOnTermination': True,
+
                         # 8gb volume
                         'VolumeSize': 8,
+
+                        # Volume type
                         'VolumeType': 'gp2',
                     },
                 },
@@ -59,7 +62,7 @@ class EC2Creator:
             self.create_instance(constant.US_EAST_1B, constant.T2_LARGE),
             self.create_instance(constant.US_EAST_1C, constant.T2_LARGE),
             self.create_instance(constant.US_EAST_1D, constant.T2_LARGE),
-            self.create_instance(constant.US_EAST_1E, constant.T2_LARGE),
+            self.create_instance(constant.US_EAST_1E, constant.T2_LARGE)
         ]
 
     def create_cluster_m4_large(self):
@@ -68,7 +71,7 @@ class EC2Creator:
             self.create_instance(constant.US_EAST_1B, constant.M4_LARGE),
             self.create_instance(constant.US_EAST_1C, constant.M4_LARGE),
             self.create_instance(constant.US_EAST_1D, constant.M4_LARGE),
-            self.create_instance(constant.US_EAST_1E, constant.M4_LARGE),
+            self.create_instance(constant.US_EAST_1E, constant.M4_LARGE)
         ]
 
     def create_clusters(self):
