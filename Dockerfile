a5a8cce171dd953af57f615cce0c69bb1922bc17
Jeremy Thai <github@jetha.anonaddy.com>
2022-10-14 22:18:59 -0400

    Refactored send request script

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,13 @@
 FROM python:3.10.0
 
 
-COPY requirements.txt requirements.txt
-
 RUN pip install requests==2.27.1
 
 COPY send_requests.py send_requests.py
 
 COPY constant.py constant.py
+
+COPY lb_address.txt lb_address.txt
+
 
 CMD [ "python",  "./send_requests.py" ]
--- send_requests.py
+++ send_requests.py
@@ -4,40 +4,45 @@ import time
 import threading
 
 
-def send_GET_request(cluster_route):
-    elb_dns = open(constant.LB_ADDRESS_PATH).readline()
-    url = f'http://{elb_dns}/{cluster_route}'
+def send_GET_request(url):
     response = requests.get(url)
 
 
-def run_test_scenario_1():
+def run_test_scenario_1(url_cluster_1, url_cluster_2):
     print("Thread 1 start")
+
     for _ in range(1000):
-        send_GET_request(cluster_route='cluster1')
-        send_GET_request(cluster_route='cluster2')
+        send_GET_request(url_cluster_1)
+        send_GET_request(url_cluster_2)
 
     print("Thread 1 end")   
 
 
-def run_test_scenario_2():
+def run_test_scenario_2(url_cluster_1, url_cluster_2):
     print("Thread 2 start")
     for _ in range(500):
-        send_GET_request(cluster_route='cluster1')
-        send_GET_request(cluster_route='cluster2')
+        send_GET_request(url_cluster_1)
+        send_GET_request(url_cluster_2)
 
     time.sleep(60)
 
     for _ in range(1000):
-        send_GET_request(cluster_route='cluster1')
-        send_GET_request(cluster_route='cluster2')
+        send_GET_request(url_cluster_1)
+        send_GET_request(url_cluster_2)
 
     print("Thread 2 end")
 
 
 if __name__ == "__main__":
 
-    thread_1 = threading.Thread(target=run_test_scenario_1)
-    thread_2 = threading.Thread(target=run_test_scenario_2)
+
+    # Fetch ELB dns address
+    elb_dns = open(constant.LB_ADDRESS_PATH).readline()
+    url_cluster_1 = f'http://{elb_dns}/cluster1'
+    url_cluster_2 = f'http://{elb_dns}/cluster2'
+
+    thread_1 = threading.Thread(target=run_test_scenario_1, args=(url_cluster_1,url_cluster_2))
+    thread_2 = threading.Thread(target=run_test_scenario_2, args=(url_cluster_1, url_cluster_2))
 
     # Start threads
     thread_1.start()
