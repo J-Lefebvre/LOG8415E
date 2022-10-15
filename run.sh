79b8668be52298488c7310889f395c6a458ebb07
Jeremy Thai <github@jetha.anonaddy.com>
2022-10-15 17:22:36 -0400

    Added run bash script to automate configuration and execution of program

---
+++ run.sh
@@ -0,0 +1,9 @@
+# Create a virtual environment
+python -m venv venv
+source venv/bin/activate
+
+# Install required dependencies
+pip install -r requirements.txt
+
+# Run main script
+python main.py
