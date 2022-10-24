6ddb35d77e128e8fc946a536a95b404a31a594f4
Milen Bozhilov <milen.i.bozhilov@gmail.com>
2022-10-24 12:29:09 -0400

    Added constant file

---
+++ TP2/constant.py
@@ -0,0 +1,11 @@
+# EC2
+UBUNTU_IMAGE = "ami-08c40ec9ead489470"
+US_EAST_1A = "us-east-1a"
+
+M4_LARGE = "m4.large"
+
+# Port configuration
+DEFAULT_SECURITY_GROUP_NAME = "default"
+CIDR_IP = "0.0.0.0/0"
+HTTP_PORT = 80
+IP_PROTOCOL = "tcp"
