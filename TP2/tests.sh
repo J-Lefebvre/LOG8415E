066ed07f31fd55ed16968772a8333f121b9a7592
Jules Lefebvre <jltlefebvre@gmail.com>
2022-10-30 10:04:39 -0400

    Added docstrings to tests.sh

--- TP2/tests.sh
+++ TP2/tests.sh
@@ -1,24 +1,28 @@
-# Save wordcount execution time of Ulysses dataset using Hadoop
+#!/bin/bash
+
+# test.sh automates the execution of the WordCount problems and stores the processing times in a text file
+
+# 1. Compute the word frequency of the Ulysses dataset using Hadoop and store the execution time
 echo -n "Hadoop - Ulysses" >> time_results.txt;
 { time hadoop jar files/wc.jar WordCount ./input/ ./output 2>1; } 2>> time_results.txt;
 
 echo "" >> time_results.txt;
 
-# Save wordcount execution time of Ulysses dataset using Linux
+# 2. Compute the word frequency of the Ulysses dataset using Linux and store the execution time
 echo -n "Linux - Ulysses" >> time_results.txt;
 { time cat files/pg4300.txt | tr ' ' '\n' | sort | uniq -c 2>1; } 2>> time_results.txt;
 
 echo "" >> time_results.txt;
 
-# Loop dataset folder and save wordcount execution time for each dataset using spark
+# 3. Compute WordCount using Spark on the datasets contained in ~/files/datasets
+#    and store the execution times.
 echo '######## SPARK ########' >> time_results.txt;
 for file in $(ls ~/files/datasets/)
 do
-    # remove file extension
-    filename=$(echo $file| cut  -d'.' -f 1);
-    echo -n $filename >> time_results.txt;
-
-    { time python3 -c "
+  # Remove file extensions
+  filename=$(echo $file| cut  -d'.' -f 1);
+  echo -n $filename >> time_results.txt;
+  { time python3 -c "
 import findspark
 import shutil
 import os
@@ -32,13 +36,15 @@ if os.path.exists('output/${filename}_spark_res/'):
 counts.saveAsTextFile('output/${filename}_spark_res/')
 sc.stop()
 spark.stop()
-"2>1; } 2>> time_results.txt;
-echo "" >> time_results.txt;
+  "2>1; } 2>> time_results.txt;
+  echo "" >> time_results.txt;
 done;
 
 echo "" >> time_results.txt;
 echo "" >> time_results.txt;
 
+# 4. Compute WordCount using Hadoop on the datasets contained in ~/files/datasets
+#    and store the execution times.
 echo '######## HADOOP ########' >> time_results.txt;
 for file in $(ls ~/files/datasets/)
 do
