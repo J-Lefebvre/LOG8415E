0088b665539f3fe38b0c035d07d60db1c74c4d15
Jeremy Thai <github@jetha.anonaddy.com>
2022-10-31 22:35:14 -0400

    Added more documentation in main script and spark script

--- TP2/WordCount.py
+++ TP2/WordCount.py
@@ -2,15 +2,20 @@ import findspark
 findspark.init()
 from pyspark.sql import SparkSession
 
+# Check if there is a default SparkSession and return that one
 spark = SparkSession.builder.master('local').appName('Firstprogram').getOrCreate()
 sc=spark.sparkContext
 
+# Read word count dataset
 text_file = sc.textFile('files/pg4300.txt')
+
+# Compute word frequency 
 counts = text_file.flatMap(lambda line: line.split(' ')).map(lambda word: (word, 1)).reduceByKey(lambda x, y: x + y).collect()
 
+# Save results
 f = open('output/spark_part-r-00000', 'w')
 f.write('\n'.join(map(lambda x: str(x), counts)))
 f.close()
-
+ 
 sc.stop()
 spark.stop()
--- TP2/main.py
+++ TP2/main.py
@@ -1,15 +1,17 @@
 from EC2_instances_creator import EC2Creator
+import constant
 import time
-import os
 
 ec2 = EC2Creator()
 
 print('Creating instance...')
-instance_id = ec2.create_m4_instance()
-print('Instance created!')
+
+instance_id = ec2.create_instance(constant.US_EAST_1A, constant.M4_LARGE)
+print(f'Instance {instance_id} created!')
 
 print("Waiting 60 seconds before continuing...")
 time.sleep(60)
 
-# Terminate services
-# ec2.terminate_instance()
+print("""Connect now to the instance to view the results of Hadoop vs Linux vs Spark and \
+execute the friendship recommendation algorithm. It may take a few minutes before \
+all the results are ready.""")
