021355dba9881b3a90e0b4fbd703a8eb93b6bdd6
Jeremy Thai <github@jetha.anonaddy.com>
2022-10-31 22:58:55 -0400

    Added back tests.sh code into launch script

--- TP2/launch_script.sh
+++ TP2/launch_script.sh
@@ -83,18 +83,73 @@ apt install python3-pip -y;
 pip install pyspark;
 pip install findspark;
 
-# 11. Execution of the Social Network problem with Hadoop
+# 11. Compute the word frequency of the Ulysses dataset using Hadoop and store the execution time
+echo -n "Hadoop - Ulysses" >> time_results.txt;
+{ time hadoop jar files/wc.jar WordCount ./input/ ./output 2>1; } 2>> time_results.txt;
+
+echo "" >> time_results.txt;
+
+# 12. Compute the word frequency of the Ulysses dataset using Linux and store the execution time
+echo -n "Linux - Ulysses" >> time_results.txt;
+{ time cat files/pg4300.txt | tr ' ' '\n' | sort | uniq -c 2>1; } 2>> time_results.txt;
+
+echo "" >> time_results.txt;
+
+# 13. Compute WordCount using Spark on the datasets contained in ~/files/datasets
+#     and store the execution times.
+echo '######## SPARK ########' >> time_results.txt;
+for file in $(ls ~/files/datasets/)
+do
+  # Remove file extensions
+  filename=$(echo $file| cut  -d'.' -f 1);
+  echo -n $filename >> time_results.txt;
+  { time python3 -c "
+import findspark
+import shutil
+import os
+from pyspark.sql import SparkSession
+spark = SparkSession.builder.master('local').appName('FirstProgram').getOrCreate()
+sc=spark.sparkContext
+text_file = sc.textFile('files/datasets/${file}')
+counts = text_file.flatMap(lambda line: line.split(' ')).map(lambda word: (word, 1)).reduceByKey(lambda x, y: x + y)
+if os.path.exists('output/${filename}_spark_res/'):
+  shutil.rmtree('output/${filename}_spark_res/')
+counts.saveAsTextFile('output/${filename}_spark_res/')
+sc.stop()
+spark.stop()"
+2>1; } 2>> time_results.txt;
+  echo "" >> time_results.txt;
+done;
+
+echo "" >> time_results.txt;
+echo "" >> time_results.txt;
+
+# 14. Compute WordCount using Hadoop on the datasets contained in ~/files/datasets
+#     and store the execution times.
+echo '######## HADOOP ########' >> time_results.txt;
+for file in $(ls ~/files/datasets/)
+do
+  echo -n $file >> time_results.txt;
+  hadoop fs -rm -r ./input/;
+  hadoop fs -rm -r ./output_$file/;
+  hdfs dfs -mkdir -p input;
+  hadoop fs -cp ~/files/datasets/$file ~/input/;
+  { time hadoop jar files/wc.jar WordCount ./input/ ./output_$file 2>1; } 2>> time_results.txt;
+  echo "" >> time_results.txt;
+done;
+
+# 15. Execution of the Social Network problem with Hadoop
 hadoop fs -cp ~/files/pg4300.txt ~/input;
 hadoop fs -cp ~/files/social_network/soc-LiveJournal1Adj.txt ~/sn_input;
 
 source ~/.profile;
 
 
-# to run the friend recommendation on ec2
-# hadoop jar files/social_network/sn.jar PeopleYouMightKnow ./sn_input/ ./sn_output
+# To run the friend recommendation on ec2
+#hadoop jar files/social_network/sn.jar PeopleYouMightKnow ./sn_input/ ./sn_output
 
 
-# to run the friend recommendation locally
+## To run the friend recommendation locally
 # cd files;
 # cd social_network;
 # hadoop com.sun.tools.javac.Main *.java;
